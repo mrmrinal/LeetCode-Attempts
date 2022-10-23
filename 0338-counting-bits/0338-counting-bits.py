b97a59750a9fc256abd557d16c0b9deb4236d73e
Mrinal Ganesh <77235299+mrmrinal@users.noreply.github.com>
2022-10-23 21:33:10 +0800

    Time: 367 ms (15.27%), Space: 20.9 MB (30.21%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,9 +2,8 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         res = []
         for i in range(n + 1):
-            temp = str(bin(i))
             curr = 0
-            for char in temp:
+            for char in bin(i):
                 if char == '1':
                     curr += 1
             res.append(curr)
