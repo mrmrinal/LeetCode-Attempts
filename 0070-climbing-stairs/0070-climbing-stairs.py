75a4ebb171f9a7e185aa3b8c59d7dbf92d02d706
Mrinal Ganesh <77235299+mrmrinal@users.noreply.github.com>
2023-08-15 15:04:21 +0800

    Time: 40 ms (71.31%), Space: 16.3 MB (30.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,11 @@
-memo = [-1] * 10000
-memo[0] = 0
-memo[1] = 1
-memo[2] = 2
 class Solution:
     
     def climbStairs(self, n: int) -> int:
-        if memo[n] != -1:
-            return memo[n]
-        else:
-            memo[n] = self.climbStairs(n - 1) + self.climbStairs(n -2)
-            return memo[n]
+        curr, next = 0, 1
+        iter = 0
+        while(iter != n):
+            curr, next = next, curr + next
+            iter += 1
+        return next
         
         
